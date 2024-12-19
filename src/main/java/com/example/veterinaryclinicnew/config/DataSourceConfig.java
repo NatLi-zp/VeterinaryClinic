8758e7a56f5262df3f25716aaddb0793d8ea84c1
NatLi-zp <natali.gordienko@gmail.com>
2024-12-19 23:02:49 +0100

    VeterinaryClinic (Liquibase) 191224_2302

--- src/main/java/com/example/veterinaryclinicnew/config/DataSourceConfig.java
+++ src/main/java/com/example/veterinaryclinicnew/config/DataSourceConfig.java
@@ -9,14 +9,14 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/veterinary_clinic");
-        dataSource.setUsername("root");
-        dataSource.setPassword("220606");
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/veterinary_clinic");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("220606");
+//        return dataSource;
+//    }
 
 }
